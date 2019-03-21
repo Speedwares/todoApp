12055b0b9c8c0db3118c96f4827e70cc3b47e50f
ovuoba sampson <ovuobasampson@gmail.com>
2019-03-21 08:15:03 -0700

    created state and added different todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js';
-import AddTodo from './components/AddTodo'
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
 
 class App extends Component {
+
+    state = {
+        todo: [
+            {
+                id: 1,
+                item: "Just the first one",
+                completed: false
+            },
+            {
+                id: 2,
+                item: "This looks like the second one",
+                completed: false
+            },
+            {
+                id: 3,
+                item: "You should probably be the third",
+                completed: false
+            }
+        ]
+    };
   render() {
     return (
       <div className="App">
         <Header/>
         <AddTodo/>
+        <Todo todo={this.state.todo}/>
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import '../index.css'
+
+class Todo extends Component{
+    render() {
+        return(
+            <div>
+                <p> Todos App </p>
+            </div>
+        )
+    }
+
+}
+
+export default Todo;
